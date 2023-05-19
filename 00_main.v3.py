e39793b4c2d0e4300173675c44956ce6d755a7a6
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2023-05-19 15:02:00 +1200

    Update

--- 00_main.v3.py
+++ 00_main.v3.py
@@ -6,6 +6,9 @@ Update: Added component 2 menu function
 instructions
 By Rafael Anggawijaya"""
 
+import random
+import time
+
 
 # Functions for a text formatter (From Lucky Unicorn pre-assessment)
 def text_formatter(symbol_1, side_symbol, symbol_2, text):
@@ -22,18 +25,18 @@ def yes_no(question_text):
     while True:
         # ask player input - (if they need instructions)
 
-        answer = input(question_text).lower()
+        answer_ = input(question_text).lower()
 
         # if player input == yes or y output - (Instructions)
 
-        if answer == "y" or answer == "yes":
-            answer = "yes"
-            return answer
+        if answer_ == "y" or answer_ == "yes":
+            answer_ = "yes"
+            return answer_
 
         # if player input == no or n output - ('Display menu')
-        elif answer == "n" or answer == "no":
-            answer = "no"
-            return answer
+        elif answer_ == "n" or answer_ == "no":
+            answer_ = "no"
+            return answer_
 
         # otherwise output - (error)
         else:
@@ -100,11 +103,109 @@ instructions_ = yes_no("Do you need Instructions? (enter y or n): ")
 # Gives instructions  then menu when the answer is yes or just menu when no
 
 if instructions_ == "yes":
+    # calls instruction function
     instructions()
+    # calls menu function
     menu_ = menu(("What mode do you want\n1. numbers(enter 1)\n2. "
                   "days(enter 2)\n:"),
                  "What difficulty do you want to play on?(easy or hard):")
 else:
+    # calls menu function
     menu_ = menu(("What mode do you want\n1. numbers(enter 1)\n2. "
                   "days(enter 2)\n:"),
                  "What difficulty do you want to play on?(easy or hard):")
+
+# calls menu function
+
+
+# list for number question and answer
+number = [["1", "tahi"], ["2", "rua"], ["3", "toru"], ["4", "wha"],
+          ["5", "rima"],
+          ["6", "ono"], ["7", "whitu"], ["8", "waru"], ["9", "iwa"], ["10",
+                                                                      "tekau"]]
+# list for number question and answer
+day = [["Monday", "Rahina"], ["Tuesday", "Ratu"], ["Wednesday", "Raapa"],
+       ["Thursday", "Rapare"], ["Friday", "Ramere"], ["Saturday", "Rahoroi"],
+       ["Sunday", "Ratapu"]]
+
+# sets score
+score = 0
+# sets question (can change to maori or english for hard mode)
+type_ = ""
+# sets time
+time_ = 0
+time.sleep(1)
+# count down
+print("\nStarting in :\n3")
+time.sleep(1)
+print(2)
+time.sleep(1)
+print(1)
+start = time.time()
+# starts game loop for certain modes and difficulty
+if menu_ == "easy number":
+    # question generator
+    random.shuffle(number)
+    for i in number:
+        # question
+        answer = input(f"What is the maori word for {i[0]}\n>")
+        # checks answer
+        if answer == i[1]:
+            print("Correct")
+            score += 1
+        else:
+            print("Wrong")
+elif menu_ == "easy day":
+    # question generator
+    random.shuffle(day)
+    for i in day:
+        # question
+        answer = input(f"What is the maori word for {i[0]}\n>").lower()
+        # checks answer
+        if answer == i[1].lower():
+            print("Correct")
+            score += 1
+        else:
+            print("Wrong")
+elif menu_ == "hard number":
+    # question generator
+    random.shuffle(number)
+    for i in number:
+        # generates a number to pick between maori and english
+        random_ = random.randint(0, 1)
+        if random_ == 0:
+            type_ = "maori"
+            correct = 1
+        else:
+            type_ = "english"
+            correct = 0
+        # question
+        answer = input(f"What is the {type_} word for"
+                       f" {i[random_]}\n>").lower()
+        # checks answer
+        if answer == i[correct].lower():
+            print("Correct")
+            score += 1
+        else:
+            print("Wrong")
+elif menu_ == "hard day":
+    # question generator
+    random.shuffle(day)
+    for i in day:
+        # generates a number to pick between maori and english
+        random_ = random.randint(0, 1)
+        if random_ == 0:
+            type_ = "maori"
+            correct = 1
+        else:
+            type_ = "english"
+            correct = 0
+        # question
+        answer = input(f"What is the {type_} word for"
+                       f" {i[random_]}\n>").lower()
+        # checks answer
+        if answer == i[correct].lower():
+            print("Correct")
+            score += 1
+        else:
+            print("Wrong")
--- 06_results_play_again.v1.py
+++ 06_results_play_again.v1.py
@@ -1,7 +1,44 @@
-"""Maori Quiz -game loop- v2
-This program is what is responsible for the game mechanics and the question
-generating. Easy is straight forward only asking the maori names while hard
-chooses between the two asking for english translation of maori words and
-vis versa  It also gives a score and times the player
-Update: Tried a different list type to do the same thing which is more
-suitable for accomplishing the task however has a lot of code"""
+"""Maori Quiz -game loop- v1
+This part shows the results after the game it also asks the player if they
+want to play again"""
+
+
+# yes_no function
+
+def yes_no(question_text):
+    while True:
+        # ask player input - (if they need instructions)
+
+        answer = input(question_text).lower()
+
+        # if player input == yes or y output - (Instructions)
+
+        if answer == "y" or answer == "yes":
+            answer = "yes"
+            return answer
+
+        # if player input == no or n output - ('Display menu')
+        elif answer == "n" or answer == "no":
+            answer = "no"
+            return answer
+
+        # otherwise output - (error)
+        else:
+            print("error -invalid input- (please enter yes/no or y/n)")
+
+
+# main routine
+
+# time input (for testing purposes what be received from game loop)
+time_ = input("time:")
+# score input (for testing purposes what be received from game loop)
+score_ = input("score:")
+# What to score out of input (for testing purposes what be received from game
+# loop)
+scored_ = input("out of:")
+print(f"You got {score_}/{scored_}\nIt took you {time_} seconds")
+play_again = yes_no("Do you want to play again?")
+if play_again == "yes":
+    print("menu")
+else:
+    print("Goodbye screen")
